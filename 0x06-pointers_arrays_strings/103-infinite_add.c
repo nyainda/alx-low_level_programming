eb10d4a0e974fe25fab03b41d92dad727bcf76f3
bruce oyugi <oyugibruce2017@gmail.com>
2022-03-24 14:05:22 +0300

    pointers, arrays and strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,53 +1,78 @@
 #include "main.h"
 
 /**
- *infinite_add - concatenates two strings
- *@n1: number 1
- *@n2: number 2
- *@r: pointer that contains sum of numbers
- *@size_r: size of buffer that contains sum of numbers
- *Return: the pointer with new number
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
  */
+
+void rev_string(char *n)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
+	}
+}
+
+/**
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
+ */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int longn1;
-	int longn2;
-	int bigger;
-	int op;
-	int ad;
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
 
-	for (longn1 = 0; n1[longn1] != '\0'; longn1++)
-	for (longn2 = 0; n2[longn2] != '\0'; longn2++)
-	if (longn1 >= longn2)
-		bigger = longn1;
-	else
-		bigger = longn2;
-	if (size_r <= (bigger + 1))
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
 		return (0);
-	while (bigger > 0)
+	while (j >= 0 || i >= 0 || overflow == 1)
 	{
-		if (longn1 >= 0 && longn2 >= 0)
-		{
-			op = (*(n1 + longn1) - '0') + (*(n2 + longn2) - '0') + ad;
-		}
+		if (i < 0)
+			val1 = 0;
 		else
-		{
-			if (longn1 < 0 && longn2 >= 0)
-				op = (*(n2 + longn2) - '0') + ad;
-			else
-			{
-				if (longn1 >= 0 && longn2 < 0)
-					op = (*(n1 + longn1) - '0') + ad;
-			}
-		}
-	*(r + bigger) = (op % 10) + '0';
-	ad = op / 10;
-	longn1--;
-	longn2--;
-	bigger--;
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
 	}
-	if (*(r + 0) == 0)
-		return (r + 1);
-	r[bigger + 1] = '\n';
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
 	return (r);
 }
