e6aaab3a30ac350025fa30f0a05ab15139c654f4
bruce oyugi <oyugibruce2017@gmail.com>
2022-03-28 16:01:48 +0300

    Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
-int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -9,7 +8,8 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
-void set_string(char **s, char *to);
+int _putchar(char c);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
 #endif
