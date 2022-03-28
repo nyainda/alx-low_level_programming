e39cc41492663e912e3b6e659ec2d37935807984
bruce oyugi <oyugibruce2017@gmail.com>
2022-03-28 15:50:07 +0300

    Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,20 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * _memset - fills memory with a constant byte
- * @s: memory space to be filled
- * @size: the size of the memory to print
- * @buffer: the address of memory to print
- *
- * Return: Nothing
+ * _memset - ironnou
+ * @s: pointer to char params
+ * @b: data to change
+ * @n: index
+ * Return: *s
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
-		*(s + i) = b;
+		s[i] = b;
 	}
 	return (s);
 }
