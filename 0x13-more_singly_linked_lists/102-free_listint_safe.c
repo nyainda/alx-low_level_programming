aaf716686f0753b583a4745f8b867eac2b44ed1b
bruce oyugi <oyugibruce2017@gmail.com>
2022-04-26 19:23:30 +0300

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,5 +1,7 @@
 #include "lists.h"
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 /**
  * free_listint_safe - frees a linked list
  * @h: pointer to the first node in the linked list
