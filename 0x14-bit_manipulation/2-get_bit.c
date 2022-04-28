4b6000907c2575711c03c9214dfc5d587f75a6c4
bruce oyugi <oyugibruce2017@gmail.com>
2022-04-28 17:15:26 +0300

    0x14. C - Bit manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * get_bit - returns the value of a bit at a given index.
- * @n: number to check bits in
- * @index: index at which to check bit
+ * get_bit - Gets the value of a bit at a given index.
+ * @n: The bit.
+ * @index: The index to get the value at - indices start at 0.
  *
- * Return: value of the bit, or -1 if there is an error
+ * Return: If an error occurs - -1.
+ *         Otherwise - The value of bit at index.
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int divisor, check;
-
-	if (index > (sizeof(unsigned long int) * 8 - 1))
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	divisor = 1 << index;
-	check = n & divisor;
-	if (check == divisor)
-		return (1);
-	return (0);
+
+	if ((n & (1 << index)) == 0)
+		return (0);
+
+	return (1);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,8 @@
-#ifndef _main_H_
-#define _main_H_
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char);
+/*prototypes*/
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
@@ -10,4 +11,4 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 
-#endif /* _main_H_ /
+#endif
