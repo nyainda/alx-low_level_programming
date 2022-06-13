00ac8078d15f94c3d9529dc9321732ae99986554
bruce oyugi <oyugibruce2017@gmail.com>
2022-06-13 18:30:54 +0300

    0x18. C - Dynamic libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp ../sh2.sh .
-. ./sh2.sh
+wget -P /tmp https://github.com/nyainda/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libwin.so
+export LD_PRELOAD=/tmp/libwinner.so
